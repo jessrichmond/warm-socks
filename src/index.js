39199cb61aa25fe105bea4b5b0ddab2976276c85
Jess Richmond <yesjessrichmond@gmail.com>
2020-12-22 16:03:28 -0500

    b

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// const express = require('express');
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`warm socks listening at http://localhost:${port}`);
+// })
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
